d5f662d1185a689c97715a351302b66b71ba5b61
sharanya <sharanya.517@gmail.com>
2017-04-18 00:33:26 -0700

    cleaned up tests

--- src/bloomFilter.py
+++ src/bloomFilter.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env
+import sys
 from math import ceil, log
 from hashlib import sha512, sha384, sha256, sha1, md5
 import bitarray
@@ -10,11 +11,11 @@ class BloomFilter:
     def __init__(self, capacity=100000, error_rate=0.001,
                  hashes=None, hash_func=sha256):
         '''initalize the filter, check validity of arguments'''
-        if capacity < 1:
+        if capacity < 1 or capacity > sys.maxsize:
             capacity = 100000
         self.__num_items__ = capacity
 
-        if error_rate <= 0 or error_rate > 1.0:
+        if error_rate <= 0 or error_rate >= 1.0:
             error_rate = 0.001
         self.__false_positive_rate__ = error_rate
 
@@ -30,11 +31,16 @@ class BloomFilter:
                 self.__num_items__)
 
         self.__hash_func__ = hash_func
+        # print(self.__hash_func__)
 
         self.__bit_array__ = bitarray.bitarray(self.__num_bits__)
         # print(self.__num_bits__, self.__num_hashes__,
         #      self.__bit_array__.__sizeof__())
         self.__bit_array__.setall(0)
+        print(self.__num_items__,
+              self.__false_positive_rate__,
+              self.__num_hashes__,
+              self.__hash_func__)
 
     def add_item(self, item):
         '''add an item to filter'''
--- test/test_bloom_filter.py
+++ test/test_bloom_filter.py
@@ -2,8 +2,9 @@ import random
 from hashlib import sha512, sha384, sha256, sha1, md5
 from pylab import plot, show, xlabel, ylabel
 import sys
-sys.path.insert(0, '/home/sharanya/Documents/'
+sys.path.insert(0, '../../'
                 'bloom_filter_python_implementation/')
+from src.bloomFilter import BloomFilter
 
 
 def generate_rand_data(n, chars):
@@ -11,20 +12,19 @@ def generate_rand_data(n, chars):
     return ''.join(random.choice(chars) for i in range(n))
 
 
-def test_bloom_filter(n, hash_func, k=None):
+def test_bloom_filter(n, hash_func, p=0):
     """ returns the percentage of false positives """
-    print(n, k)
-    bloom = BloomFilter(capacity=n, hashes=k, )
-    rand_keys = [generate_rand_data(10, 'abcde') for i in range(n)]
-    # pushing the items into the data structure
-    for rk in rand_keys:
-        bloom.add_item(rk)
+    bloom = BloomFilter(capacity=n, hash_func=hash_func, error_rate=p)
+    rand_data = [generate_rand_data(10, 'abcde') for i in range(n)]
+    for rd in rand_data:
+        bloom.add_item(rd)
     # adding other elements to the dataset
-    rand_keys = rand_keys + [generate_rand_data(10, 'fghil') for i in range(n)]
+    rand_data = rand_data + [generate_rand_data(10, 'fghij')
+                             for i in range(n)]
     # performing a query for each element of the dataset
     false_positive = 0
-    for rk in rand_keys:
-        if not bloom.maybe_contains(rk):
+    for rd in rand_data:
+        if not bloom.maybe_contains(rd):
             false_positive += 1
     return float(false_positive)/n*100.0
 
@@ -32,7 +32,14 @@ if __name__ == '__main__':
     ''' testing the filter '''
     hash_functions = [sha512, sha384, sha256, sha1, md5]
     hash_func_names = ['sha512', 'sha384', 'sha256', 'sha1', 'md5']
-    err_rates = [test_bloom_filter(n=10000, hash_func=hash_fn)
+    err_rates = [test_bloom_filter(n=100000, hash_func=hash_fn)
                  for hash_fn in hash_functions]
     print(hash_func_names, "\n", err_rates)
+    print("\n\n")
+    p_values = [0.0000001, 0.000001, 0.00001, 0.0001, 0.001, 0.01, 0.1,
+                0.25, 0.75, 0.9]
+    for p in p_values:
+        err_rates = [test_bloom_filter(n=100000, hash_func=hash_fn, p=p)
+                     for hash_fn in hash_functions]
+        print(p, hash_func_names, "\n", err_rates, "\n")
 

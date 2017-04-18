0e81792e2fb090de7fd25e601af635acf241d704
sharanya <sharanya.517@gmail.com>
2017-04-17 23:44:45 -0700

    plotting test results

--- src/bloomFilter.py
+++ src/bloomFilter.py
@@ -1,29 +1,29 @@
 #!/usr/bin/env
 from math import ceil, log
-from hashlib import sha256
+from hashlib import sha512, sha384, sha256, sha1, md5
 import bitarray
 # import mmh3 # broken, doesn't seem to work
 
 
 class BloomFilter:
 
-    def __init__(self, capacity=100000, error_rate=0.001, hashes=None):
+    def __init__(self, capacity=100000, error_rate=0.001,
+                 hashes=None, hash_func=sha256):
         self.__num_items__ = capacity
         self.__false_positive_rate__ = error_rate
-        # self.__hash_func__ = None
-        # self.__log2_num_bits__ = 31
+        self.__hash_func__ = hash_func
         self.__num_bits__ = ceil(
             (self.__num_items__ * log(self.__false_positive_rate__)) /
             log(1.0 / (pow(2.0, log(2.0)))))
-        if hashes:
+        if isinstance(hashes, int):
             self.__num_hashes__ = hashes
         else:
             self.__num_hashes__ = round(
                 log(2.0) * self.__num_bits__ /
                 self.__num_items__)
         self.__bit_array__ = bitarray.bitarray(self.__num_bits__)
-        # print(self.__num_bits__, self.__num_hashes__,
-        #      self.__bit_array__.__sizeof__())
+        print(self.__num_bits__, self.__num_hashes__,
+              self.__bit_array__.__sizeof__())
         self.__bit_array__.setall(0)
 
     def add_item(self, item):
@@ -46,7 +46,7 @@ class BloomFilter:
         pass
 
     def __get_hashed_index__(self, item, n, filter_size):
-        digest = sha256(item.encode('utf-8')).hexdigest()
+        digest = self. __hash_func__(item.encode('utf-8')).hexdigest()
         hashA = digest[0:len(digest)//2]
         hashB = digest[len(digest)//2:]
         hc = int(hashA, 16) + n*int(hashB, 16)
--- test/test_bloom_filter.py
+++ test/test_bloom_filter.py
@@ -1,27 +1,30 @@
 import random
+from hashlib import sha512, sha384, sha256, sha1, md5
 from pylab import plot, show, xlabel, ylabel
 import sys
 sys.path.insert(0, '/home/sharanya/Documents/'
                 'bloom_filter_python_implementation/')
 from src.bloomFilter import BloomFilter
+import matplotlib.pyplot as plt; plt.rcdefaults()
+import numpy as np
+import matplotlib.pyplot as plt
 
 
-def rand_data(n, chars):
-    """generate random strings using the characters in chars """
+def generate_rand_data(n, chars):
+    """generates random strings using the characters in chars """
     return ''.join(random.choice(chars) for i in range(n))
 
 
-def bloomTest(n, k=None):
-    """ return the percentage of false positive """
+def test_bloom_filter(n, hash_func, k=None):
+    """ returns the percentage of false positive """
     print(n, k)
-    bloom = BloomFilter(capacity=n, hashes=k)
-    # generating a random data
-    rand_keys = [rand_data(10, 'abcde') for i in range(n)]
+    bloom = BloomFilter(capacity=n, hashes=k, )
+    rand_keys = [generate_rand_data(10, 'abcde') for i in range(n)]
     # pushing the items into the data structure
     for rk in rand_keys:
         bloom.add_item(rk)
     # adding other elements to the dataset
-    rand_keys = rand_keys + [rand_data(10, 'fghil') for i in range(n)]
+    rand_keys = rand_keys + [generate_rand_data(10, 'fghil') for i in range(n)]
     # performing a query for each element of the dataset
     false_positive = 0
     for rk in rand_keys:
@@ -31,21 +34,16 @@ def bloomTest(n, k=None):
 
 if __name__ == '__main__':
     # testing the filter
-    def get_err_rates(start, end, factor):
-        res = []
-        while start <= end:
-            res.append(start)
-            start *= factor
-        return res
     n = 10000
-    k = range(1, 64)
-    perc = [bloomTest(n=n, k=kk) for kk in k] # k is varying
-    rates = get_err_rates(0.0000001, 1, 10)
-    # perc = [bloomTest(n, p) for p in rates]
-    print(rates,"\n",  perc)
+    hash_functions = [sha512, sha384, sha256, sha1, md5]
+    hash_func_names = ['sha512', 'sha384', 'sha256', 'sha1', 'md5']
+    y_pos = np.arange(len(hash_func_names))
+    err_rates = [test_bloom_filter(n=10000, hash_func=hash_fn)
+                 for hash_fn in hash_functions]
+    print(hash_func_names, "\n", err_rates)
     # plotting the result of the test
-
-    plot(k, perc, '--ob', alpha=.7)
-    ylabel('false positive %')
-    xlabel('k')
-    show()
+    plt.bar(y_pos, err_rates, align='center', alpha=0.5)
+    plt.xticks(y_pos, hash_func_names)
+    plt.ylabel('False positive rates')
+    plt.title('Hash functions performance')
+    plt.show()
